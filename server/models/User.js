3ac8550436c0d7586646700fae58b46bce434780
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-07 18:36:03 -0500

    add properties to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,36 @@
 const { model, Schema } = require('mongoose');
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 5,
+      },
+      applications: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Application'
+        }
+      ],
+      adoptions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Animal'
+        }
+      ]
+});
 
 const User = model('User', userSchema);
 
