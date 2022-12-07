d9bd4f55a1c8da9bea0b8ad6639ff395815f26da
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-07 18:44:22 -0500

    add properties to application model

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,6 +1,15 @@
 const { model, Schema } = require('mongoose');
 
-const applicationSchema = new Schema({});
+const applicationSchema = new Schema({
+    applicant: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    adoptee: {
+        type: Schema.Types.ObjectId,
+        ref: 'Animal'
+    }
+});
 
 const Application = model('Application', applicationSchema);
 
