7c73a07ef664a8384060f8fafd899d7155901b8a
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-07 18:40:50 -0500

    implement password encryption

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { model, Schema } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -31,6 +32,19 @@ const userSchema = new Schema({
         }
       ]
 });
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
