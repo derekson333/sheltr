b1ccdc7702354a03f8a00d1a8ec6a86c5a35b306
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-07 19:41:15 -0500

    add initial typeDefs and query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,23 @@
 const { gql } = require('apollo-server-express')
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        applications: [Application]
+        adoptions: [Animal]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+    }
+`;
 
 module.exports = typeDefs;
