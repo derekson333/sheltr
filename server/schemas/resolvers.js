f840334e6260b655bcf533b4a20a04addec44746
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-07 19:18:32 -0500

    import password functions into resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Animal, Application } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
