e6745156746941e3cb1d4e8af3df0547fda9e868
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-12-08 20:07:01 -0500

    Imported css, customized nav (subject to change)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
 function Header() {
   return (
-    <h1 id="name" className="navbar-brand">
-      Animals
-    </h1>
+    <>
+      <h1 id="heading" className="navbar-brand">
+        Animals
+      </h1>
+      <span class="navbar-text">Adopt a pet today!</span>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </>
   );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,47 +1,83 @@
 import React from "react";
-
+import "../css/styles.css";
 function Nav({ currentPage, pageChange }) {
   return (
-    <ul style={{ float: "right" }} className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => pageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#adopt"
-          onClick={() => pageChange("Adopt")}
-          className={
-            currentPage === "Adopt" ? "nav-link active" : "nav-link"
-          }
-        >
-          Adopt
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => pageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => pageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </a>
-      </li>
-    </ul>
+    <>
+      <div className="margin-left collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a
+              href="#about"
+              onClick={() => pageChange("About")}
+              className={
+                currentPage === "About" ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#adopt"
+              onClick={() => pageChange("Adopt")}
+              className={
+                currentPage === "Adopt" ? "nav-link active" : "nav-link"
+              }
+            >
+              Adopt
+            </a>
+          </li>
+          <li className="nav-item">
+            {" "}
+            <a
+              href="#contact"
+              onClick={() => pageChange("Contact")}
+              className={
+                currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Account
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="#">
+                Login
+              </a>
+              <a className="dropdown-item" href="#">
+                Logout
+              </a>
+              <div class="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">
+                Signup
+              </a>
+            </div>
+          </li>
+        </ul>
+        <form id = 'search' className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+          <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">
+            Search a pet
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
