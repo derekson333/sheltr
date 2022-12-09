66ae74e857d258924accb42c5f30d953d5726f4f
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-12-08 21:16:05 -0500

    Fixed non responsive dropdown + expandable nav. Added react-bootstrap to dependencies

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Nav from "./Nav";
 import Header from "./Header";
 import About from "./pages/About";
 import Adopt from "./pages/Adopt";
 import Contact from "./pages/Contact"
+import Donate from "./pages/Donate"
 import Footer from "./Footer";
 
 export default function Container() {
@@ -18,6 +18,9 @@ export default function Container() {
     if (currentPage === "Contact") {
       return <Contact />;
     }
+    if (currentPage === "Donate") {
+      return <Donate />;
+    }
     return <About />;
   };
 
@@ -31,8 +34,7 @@ export default function Container() {
     <div>
       <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Header />
-          <Nav currentPage={currentPage} pageChange={handlePageChange} />
+          <Header currentPage={currentPage} pageChange={handlePageChange}/>
         </nav>
       </header>
       {renderContent()}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,108 @@
 import React from "react";
-function Header() {
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../css/styles.css";
+
+function Header({ currentPage, pageChange }) {
   return (
     <>
-      <h1 id="heading" className="navbar-brand">
-        Animals
-      </h1>
-      <span class="navbar-text">Adopt a pet today!</span>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <Navbar id='header' bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand id="heading" href="#">
+            Adoptimals
+          </Navbar.Brand>
+          <span class="navbar-text">Adopt a pet today!</span>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link
+                href="#about"
+                onClick={() => pageChange("About")}
+                className={
+                  currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </Nav.Link>
+              <Nav.Link
+                href="#adopt"
+                onClick={() => pageChange("Adopt")}
+                className={
+                  currentPage === "Adopt" ? "nav-link active" : "nav-link"
+                }
+              >
+                Adopt
+              </Nav.Link>
+              <Nav.Link
+                href="#contact"
+                onClick={() => pageChange("Contact")}
+                className={
+                  currentPage === "Contact" ? "nav-link active" : "nav-link"
+                }
+              >
+                Contact
+              </Nav.Link>
+              <Nav.Link
+                href="#donate"
+                onClick={() => pageChange("Donate")}
+                className={
+                  currentPage === "Donate" ? "nav-link active" : "nav-link"
+                }
+              >
+                Donate
+              </Nav.Link>
+              <NavDropdown
+                variant="outline-success"
+                title="Account"
+                id="navbarScrollingDropdown"
+              >
+                <NavDropdown.Item
+                  onClick={() => pageChange("Login")}
+                  href="#login"
+                >
+                  Login
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={() => pageChange("Logout")}
+                  href="#logout"
+                >
+                  Logout
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={() => pageChange("Profile")}
+                  href="#profile"
+                >
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  onClick={() => pageChange("Signup")}
+                  href="#signup"
+                >
+                  Signup
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search a pet"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
