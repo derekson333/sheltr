cd721957bac036afb7cf131413c7cf40ba77c9c9
rforaker <rtforaker@gmail.com>
2022-12-11 18:14:30 -0500

    Setup clientside animal data query

--- client/src/components/utils/queries.js
+++ client/src/components/utils/queries.js
@@ -2,8 +2,15 @@ import { gql } from '@apollo/client';
 
 export const QUERY_ANIMALS = gql`
   # create a GraphQL query to be executed by Apollo Client
-  query getAnimals {
+  query Animals {
     animals {
+      name
+      sex
+      familyFriendly
+      breed
+      animalType
+      age
+      _id
     }
   }
 `;
