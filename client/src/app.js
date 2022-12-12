35a2cef51196f381eabdc3039a718b34412a95db
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-12-11 20:53:30 -0500

    Fixing accidental missing app.js

---
+++ client/src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "./components/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const App = () => <Container />;
+
+export default App;
