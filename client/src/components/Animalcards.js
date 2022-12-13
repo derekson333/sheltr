fce39aed1c35910c221e3d42ea5d12ff5079a66e
rforaker <rtforaker@gmail.com>
2022-12-12 20:12:37 -0500

    Updated styling to size images

--- client/src/components/Animalcards.js
+++ client/src/components/Animalcards.js
@@ -5,13 +5,13 @@ const Animalcards = ({ animals }) => {
         <>
             {animals.map((animal) => (
                 <div className="container" style={{ maxWidth: '70%', maxHeight: '', marginTop: '20px' }}>
-                    <div className="card text-center"><img alt="an adoptable pet" className="card-img-top w-100 d-block"></img>
+                    <div className="card text-center"><img alt="an adoptable pet" className="card-img-top w-100 d-block animalcardimg" src={animal.imgUrl}></img>
                         <div className="card-body">
                             <h4 className="card-title">{animal.name}</h4>
                             <p className="card-text" style={{ marginBottom: "5px;" }}>Type: {animal.animalType}</p>
-                            <p className="card-text" style={{ marginBottom: "5px;" }}>Age: {animal.age}</p>
-                            <p className="card-text" style={{ marginBottom: "5px;" }}>Sex: {animal.sex}</p>
-                            <p className="card-text" style={{ marginBottom: "15px;" }}>Breed: {animal.breed}</p><button className="btn btn-primary" type="button">Adopt Me!</button>
+                            <p className="card-text" style={{ marginBottom: "5px" }}>Age: {animal.age}</p>
+                            <p className="card-text" style={{ marginBottom: "5px" }}>Sex: {animal.sex}</p>
+                            <p className="card-text" style={{ marginBottom: "15px" }}>Breed: {animal.breed}</p><button className="btn btn-primary" type="button">Adopt Me!</button>
                         </div>
                     </div>
                 </div>
