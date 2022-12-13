70650792e343bf229806afd0d3d2adce9d6f2b39
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-12-12 19:28:42 -0500

    Update mutations.js
    Added comma

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
   mutation login($username: String!, $password: String!) {
-    login(username: $username password: $password) {
+    login(username: $username, password: $password) {
       token
       user {
         _id
