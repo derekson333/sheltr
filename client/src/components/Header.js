246823d23f2e81c2617916847b8dcdabac1fdfee
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-12-15 19:59:16 -0500

    cleaned up header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import Auth from "../utils/auth";
 function Header({ currentPage, pageChange }) {
   return (
